2d39fffd73f8c11583bca3902b481a3e8f010ccd
tduncan14 <nbatre1@yahoo.com>
2023-10-08 01:25:06 -0700

    keep moving forward

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,89 @@
-from fastapi import FastAPI
+#uvicorin main:app
+#uvicorn main:app --reload
+
+from fastapi import FastAPI, File,UploadFile,HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+
+#custrom Functions import
+#....
+from functions.openai_requests import convert_audio_to_text
+
+
+openai.organization = config("OPEN_AI_ORG")
+openai.api_key = config("OPEN_AI_KEY")
+
+
+
+#Initiate App
+
+
 
 app = FastAPI()
 
 
+
+#Cors -origin
+
+
+origins = [
+    "http://localhost:5173"
+    "http://localhost:5174"
+    "http://localhost:4173"
+    "http://localhost:4174"
+    "http://localhost:3000"
+]
+
+
+# corss middle ware {
+# }
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+# check healthy
 @app.get("/")
-async def root():
+async def check_health():
     print('this is treek')
-    return {"message": "Hello World"}
+    return {"message": "healthy"}
+
+
+# post reponse
+# Not playing in browser when using post request return audio
+
+
+#Get audio
+@app.get("/post-audio-get/")
+async def get_audio():
+    # get saved audio
+    audio_input = open("voice.mp3" ,'rb')
+
+    #decode audio
+    message_decoded = convert_audio_to_text(audio_input)
+
+    print(message_decoded)
+    
+    return "done"
+
+# @app.post("/post-audio/")
+# async def post_audio(file:UploadFile = File(...)):
+#  print('hello')
+
+
+
+
+# @app.get('/wealthy')
+# async def check_wealthy():
+#      return{"message":"wealthy"}
+
+
