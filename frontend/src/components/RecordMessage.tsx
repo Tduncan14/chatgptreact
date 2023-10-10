19800db71ec729d628cf7fcd8ed30b8b962749d2
tduncan14 <nbatre1@yahoo.com>
2023-10-10 11:59:25 -0700

    keep moving forward

--- frontend/src/components/RecordMessage.tsx
+++ frontend/src/components/RecordMessage.tsx
@@ -19,9 +19,14 @@ const RecordMessage = ({handleStop}:Props) => {
          render={({status,startRecording,stopRecording}) => 
          <div className="mt-2"> <button onMouseDown={startRecording} 
          onMouseUp={stopRecording}
-         className="bg-white p-4 rounded-full"> Icon</button></div>}
+         className="bg-white p-4 rounded-full"> 
+         <RecordIcon classText={status=="recording" ? "animate-pulse text-red-500 ": "text-sky-500"} /></button>
+         <p className="mt-2 text-white font-light">{status}</p>
+         </div>}
         
         />
+
+
         </>
        )
     
